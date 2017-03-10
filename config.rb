50904de4374ec192b2f112b885694166eab5bc3a
Januar Fonti <januar.fonti@dwp.co.id>
2017-03-10 15:20:15 +0700

    initial

---
+++ config.rb
@@ -0,0 +1,59 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+activate :i18n, :langs => [:id,:en], :mount_at_root => :false
+
+[:id, :en].each do |locale|
+  data.send(I18n.locale.to_sym).people.each do |jobs|
+    proxy "/#{locale}/#{jobs.link}", "/jobs.html",
+    :locals => {
+      :jobs => jobs.title,
+      :desc => jobs.desc,
+      :locale => locale
+    }
+  end
+end
+
+
+
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
